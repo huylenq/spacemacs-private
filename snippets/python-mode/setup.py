7154b81e9e3c25690a5cd68eb4ed068d763384d3
Huy Le <huy.lenq@gmail.com>
2018-08-23 15:28:57 +0700

    Initialize

---
+++ snippets/python-mode/setup.py
@@ -0,0 +1,17 @@
+# key: setup
+# name: setup.py
+# --
+from setuptools import setup
+
+setup(name='$0',
+      version='0.1.0',
+      description='$1',
+      url='http://github.com/huylenq/$0',
+      author='Flying Circus',
+      author_email='$2',
+      # license='MIT',
+      packages=['$0'],
+      install_requires=[
+
+      ],
+      zip_safe=False)
